6e9d961cd7f065611d5a8fea3c262a0d356f1743
pawelstasiewicz <pst357357@gmail.com>
2023-04-12 10:12:17 +0200

    Prepare inputs in JS

--- script/script.js
+++ script/script.js
@@ -2,13 +2,18 @@ const day = document.querySelector('#day');
 const month = document.querySelector('#month');
 const year = document.querySelector('#year');
 const btnArrow = document.querySelector('.arrow');
-const errInfo = document.createElement('p');
 
 const date = new Date();
 
+const errInfo1 = document.createElement('p');
 const err1 = document.querySelector('.err1');
+const errInfo2 = document.createElement('p');
 const err2 = document.querySelector('.err2');
+const errInfo3 = document.createElement('p');
 const err3 = document.querySelector('.err3');
+errInfo1.textContent = 'This field is required';
+errInfo2.textContent = 'This field is required';
+errInfo3.textContent = 'This field is required';
 
 const main = () => {
 	DOMEvents();
@@ -22,17 +27,40 @@ const DOMEvents = () => {
 
 const pressBtn = () => {
 	const redD = document.querySelector('.redDay');
+	const redM = document.querySelector('.redMonth');
+	const redY = document.querySelector('.redYear');
 
 	if (day.value > 31 || day.value === '') {
 		redD.classList.add('redColor');
 		day.classList.add('dayErr');
-		errInfo.textContent = 'This field is required';
-		errInfo.classList.add('errInfoColor');
-		err1.append(errInfo);
+		errInfo1.classList.add('errInfoColor');
+		err1.append(errInfo1);
 	} else if (day.value !== '') {
-		day.classList.remove('redErr');
-		err1.remove;
+		day.classList.remove('dayErr');
+		err1.textContent = '';
 		redD.classList.remove('redColor');
+	}
+
+	if (month.value > 12 || month.value === '') {
+		redM.classList.add('redColor');
+		month.classList.add('dayErr');
+		errInfo2.classList.add('errInfoColor');
+		err2.append(errInfo2);
+	} else if (month.value !== '') {
+		month.classList.remove('dayErr');
+		err2.textContent = '';
+		redM.classList.remove('redColor');
+	}
+
+	if (year.value > date.getFullYear() || year.value === '') {
+		redY.classList.add('redColor');
+		year.classList.add('dayErr');
+		errInfo3.classList.add('errInfoColor');
+		err3.append(errInfo3);
+	} else if (year.value !== '') {
+		year.classList.remove('dayErr');
+		err3.textContent = '';
+		redY.classList.remove('redColor');
 	}
 };
 
